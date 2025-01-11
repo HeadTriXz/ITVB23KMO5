ea9afd4e390b39fb6b75a9437ff03b24641caa6f
HeadTriXz <l.knol@outlook.com>
2025-01-11 18:41:20 +0100

    feat: add quick-access buttons for top brands

--- apps/app/src/app/(tabs)/(home)/index.tsx
+++ apps/app/src/app/(tabs)/(home)/index.tsx
@@ -1,14 +1,18 @@
 import type { APIGetCarResult } from "@/types/api";
+import type { CarBrand } from "@/constants/cars";
 
-import { ActivityIndicator, FlatList, StyleSheet } from "react-native";
+import { ActivityIndicator, FlatList, StyleSheet, View } from "react-native";
 import { ThemedText, ThemedView } from "@/components/base";
 
 import { AvailableCarCard } from "@/components/cards/AvailableCarCard";
 import { Header } from "@/components/layout/header";
 import { SearchWithFilter } from "@/components/common/forms";
-import { useCallback } from "react";
+import { TopBrandCard } from "@/components/cards/TopBrandCard";
+import { createParamsFromFilters } from "@/utils/filterParams";
 import { useCarSearch } from "@/hooks/useCarSearch";
 import { useRouter } from "expo-router";
+
+import React from "react";
 
 export default function HomeScreen() {
     const router = useRouter();
@@ -18,17 +22,25 @@ export default function HomeScreen() {
         pageSize: 5
     })
 
-    const onFilter = useCallback(() => {
+    const onFilter = () => {
         router.push("/(tabs)/(home)/(search)/filter");
-    }, []);
+    };
 
-    const onCarPress = useCallback((car: APIGetCarResult) => {
+    const onCarPress = (car: APIGetCarResult) => {
         router.push(`/(tabs)/(home)/(car)/${car.id}`);
-    }, []);
+    };
 
-    const onSearchFocus = useCallback(() => {
+    const onSearchFocus = () => {
         router.push("/(tabs)/(home)/(search)/search");
-    }, []);
+    };
+
+    const onBrandPress = (brand: CarBrand) => {
+        const params = createParamsFromFilters({ brands: [brand] });
+        router.push({
+            pathname: "/(tabs)/(home)/(search)/results",
+            params: params
+        });
+    }
 
     if (search.isLoading) {
         return (
@@ -43,13 +55,24 @@ export default function HomeScreen() {
         <ThemedView style={styles.container}>
             <Header withNotificationsButton />
             <SearchWithFilter onFilter={onFilter} onFocus={onSearchFocus} />
-            <ThemedText variant="headingMedium" style={styles.heading}>Available Cars</ThemedText>
             <FlatList
                 data={search.cars}
                 contentContainerStyle={styles.contentContainer}
                 keyExtractor={(item) => item.id.toString()}
                 renderItem={({ item }) => (
                     <AvailableCarCard car={item} onPress={() => onCarPress(item)} />
+                )}
+                ListHeaderComponent={(
+                    <>
+                        <ThemedText variant="headingMedium" style={styles.heading}>Top Brands</ThemedText>
+                        <View style={styles.brandsContainer}>
+                            <TopBrandCard image="bmw" onPress={() => onBrandPress("BMW")} />
+                            <TopBrandCard image="mercedes" onPress={() => onBrandPress("Mercedes-Benz")} />
+                            <TopBrandCard image="nissan" onPress={() => onBrandPress("Nissan")} />
+                            <TopBrandCard image="toyota" onPress={() => onBrandPress("Toyota")} />
+                        </View>
+                        <ThemedText variant="headingMedium" style={styles.heading}>Available Cars</ThemedText>
+                    </>
                 )}
                 refreshing={search.isLoading}
                 onRefresh={search.refresh}
@@ -59,6 +82,11 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
+    brandsContainer: {
+        flexDirection: "row",
+        gap: 10,
+        marginBottom: 15
+    },
     centered: {
         alignItems: "center",
         justifyContent: "center"
